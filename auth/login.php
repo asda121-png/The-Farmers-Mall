420a9412b5dfabecdbcd47265d318d1fb8115550
Your Name <shekinahabegailgjaljis@gmail.com>
2025-11-22 11:53:59 +0800

    update login

--- auth/login.php
+++ auth/login.php
@@ -6,9 +6,7 @@ $login_status = '';
 $login_message = '';
 $redirect_url = '';
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['login_submitted'])) {
-    $input_identifier = trim($_POST['email'] ?? '');
-    $password = $_POST['password'] ?? '';
+
 
     // Check for admin credentials
     if ($input_identifier === 'Admin1234@gmail.com' && $password === 'Admin123') {
@@ -29,7 +27,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['login_submitted'])) {
         $login_message = 'Login successful! Redirecting to homepage...';
         $redirect_url = '../user/user-homepage.html';
     }
-}
+
 // PHP SCRIPT END
 ?>
 <!DOCTYPE html>
