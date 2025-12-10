1a25c12c6397957e90b6af2afce297c13c832834
Jayson Bustamante <jayson@example.com>
2025-12-10 19:58:35 +0800

    login fix

--- auth/login.php
+++ auth/login.php
@@ -82,12 +82,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['login_submitted'])) {
                             if ($user['user_type'] === 'admin') {
                                 send_json_response('success', 'Admin login successful! Redirecting...', '../admin/admin-dashboard.php');
                             } elseif ($user['user_type'] === 'retailer') {
-<<<<<<< HEAD
+
                                 $login_message = 'Retailer login successful! Redirecting to dashboard...';
                                 $redirect_url = '../retailer/retailer-dashboard2.php';
-=======
+
                                 send_json_response('success', 'Retailer login successful! Redirecting...', '../retailer/retailerdashboard.php');
->>>>>>> 8d312e3155deedf6f28d624b227b7589d5827f1b
+
                             } else {
                                 // Default to customer
                                 send_json_response('success', 'Login successful! Redirecting...', '../user/user-homepage.php');
